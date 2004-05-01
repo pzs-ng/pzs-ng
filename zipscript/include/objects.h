b986171a9f31e2837d1065e985c109c3c3d52ed8
psxc <psxc@pzs-ng.com>
2004-05-01 20:05:09 +0000

    "fixed" objects.h

--- zipscript/include/objects.h
+++ zipscript/include/objects.h
@@ -27,7 +27,7 @@ struct GROUPINFO {
 };
 
 
-
+#if ( for_glftpd2 == FALSE )
 struct ONLINE {
  char				tagline[64];	/* The users tagline */
  char   			username[24];	/* The username of the user */
@@ -40,7 +40,22 @@ struct ONLINE {
  unsigned long			bytes_xfer;	/* Bytes transferred this far. */
  pid_t				procid;		/* The processor id of the process */
 };
-
+#else
+struct ONLINE {
+ char			tagline[64];		/* The users tagline */
+ char			username[24];		/* The username of the user */
+ char			status[256];		/* The status of the user, idle, RETR, etc */
+ short int		ssl_flag;		/* 0 = no ssl, 1 = ssl on control, 2 = ssl on control and data */
+ char			host[256];		/* The host the user is comming from (with ident) */
+ char			currentdir[256];	/* The users current dir (fullpath) */
+ long			groupid;		/* The groupid of the users primary group */
+ time_t			login_time;		/* The login time since the epoch (man 2 time) */
+ struct timeval		tstart;			/* replacement for last_update. */
+ struct timeval		txfer;			/* The time of the last succesfull transfer. */
+ unsigned long long	bytes_xfer;		/* bytes transferred so far. */
+ pid_t			procid;			/* The processor id of the process */
+};
+#endif
 
 
 struct audio {
