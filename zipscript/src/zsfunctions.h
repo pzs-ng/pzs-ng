f1a6c1158d97f3404e403ec22a1e8608b72edd68
daxxar <daxxar@pzs-ng.com>
2004-05-02 21:20:24 +0000

    Linux/BSD warning removal! (removed BOTH this time)

--- zipscript/src/zsfunctions.h
+++ zipscript/src/zsfunctions.h
@@ -126,15 +126,21 @@ unsigned long hexstrtodec(unsigned char *s) {
 /*
  * dangling links
  */
-int selector (const struct dirent *d)
-{
+#ifdef __linux__
+int selector (const struct dirent *d) {
+#else
+int selector (struct dirent *d) {
+#endif
         struct stat st;
         if ((stat(d->d_name, &st) < 0)) return 0;
         return 1;
 }
 
-int selector2 (const struct dirent *d)
-{
+#ifdef __linux__
+int selector2 (const struct dirent *d) {
+#else
+int selector2 (struct dirent *d) {
+#endif
         struct stat st;
         if ((stat(d->d_name, &st) < 0) || S_ISDIR(st.st_mode)) return 0;
         return 1;
