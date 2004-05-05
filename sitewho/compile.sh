02cc78a40983f8e997c0a0bfd4dcdf25db8afd47
psxc <psxc@pzs-ng.com>
2004-05-05 19:26:05 +0000

    sitewho support for glftpd2

--- sitewho/compile.sh
+++ sitewho/compile.sh
@@ -1,11 +1,26 @@
-echo -n "Compiling sitewho.c :"
-gcc -O2 sitewho.c -O2 -static -o sitewho 2> /dev/null
+syst=`uname -s`
+glver="`grep -r "^#define for_glftpd2" sitewho.c | awk '{print $3}'`"
+if [ $glver -eq 1 ]; then
+ glver="Glftpd v2.xx"
+else
+ glver="Glftpd v1.xx"
+fi
+echo -n "Compiling sitewho.c for $glver on $syst:"
+case $syst in
+ [Ll][Ii][Nn][Uu][Xx])
+  gcc -O2 sitewho.c -O2 -o sitewho 2> /dev/null
+  ;;
+ *[Bb][Ss][Dd]*)
+  gcc -O2 sitewho.c -O2 -static -o sitewho 2> /dev/null
+  ;;
+esac
+
 if [ $? -gt 0 ] ; then
  echo " [FAIL]"
  exit 2
 fi
 echo " [OK]"
 
-echo ""
+echo "File "
 echo "Check README, if you want to learn how this works!"
 echo ""
--- sitewho/sitewho.c
+++ sitewho/sitewho.c
@@ -1,5 +1,6 @@
-#define KEY	 0x0000DEAD   
-#define GLGROUP  "/etc/group"
+#define KEY		0x0000DEAD   	/* ipc_key */
+#define GLGROUP		"/etc/group"	/* glftpd's group-file (under chroot) */
+#define for_glftpd2	1		/* Compile for glftpd2? 1=YES, 0=NO */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,21 +14,35 @@
 #include <sys/stat.h>
 
 
+#if (for_glftpd2 == 0)
 struct ONLINE {
-  char	 tagline[64];		   /* The users tagline */
-  char   username[24];             /* The username of the user */
-  char   status[256];              /* The status of the user, idle, RETR, etc */
-  short  int ssl_flag;             /* 0 = no ssl, 1 = ssl on control, 2 = ssl on control and data */
-  char   host[256];                /* The host the user is comming from (with ident) */
-  char   currentdir[256];          /* The users current dir (fullpath) */
-  long   groupid;                  /* The groupid of the users primary group */
-  time_t login_time;               /* The login time since the epoch (man 2 time) */
-  struct timeval tstart;           /* replacement for last_update. */
-  struct timeval txfer;            /* The time of the last succesfull transfer. */
-  unsigned long long bytes_xfer;   /* bytes transferred so far. */
-  pid_t  procid;                   /* The processor id of the process */
+ char		tagline[64];	/* The users tagline */
+ char		username[24];	/* The username of the user */
+ char		status[256];	/* The status of the user, idle, RETR, etc */
+ char		host[256];	/* The host the user is comming from (with ident) */
+ char		currentdir[256];/* The users current dir (fullpath) */
+ long		groupid;	/* The groupid of the users primary group */
+ time_t		login_time;	/* The login time since the epoch (man 2 time) */
+ struct timeval	tstart;		/* Replacement for last_update. */
+ unsigned long	bytes_xfer;	/* Bytes transferred this far. */
+ pid_t		procid;		/* The processor id of the process */
+} __attribute__ ((deprecated));
+#else
+struct ONLINE {
+ char		tagline[64];	/* The users tagline */
+ char		username[24];	/* The username of the user */
+ char		status[256];	/* The status of the user, idle, RETR, etc */
+ short		int ssl_flag;	/* 0 = no ssl, 1 = ssl on control, 2 = ssl on control and data */
+ char		host[256];	/* The host the user is comming from (with ident) */
+ char		currentdir[256];/* The users current dir (fullpath) */
+ long		groupid;	/* The groupid of the users primary group */
+ time_t		login_time;	/* The login time since the epoch (man 2 time) */
+ struct timeval	tstart;		/* replacement for last_update. */
+ struct timeval	txfer;		/* The time of the last succesfull transfer. */
+ unsigned long long bytes_xfer;	/* bytes transferred so far. */
+ pid_t		procid;		/* The processor id of the process */
 };
-
+#endif
 
 struct GROUP {
 	char		*name;
