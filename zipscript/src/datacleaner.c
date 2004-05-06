3be54a8e1fd0248cc3fbe1e12aabe4e169eae928
daxxar <daxxar@pzs-ng.com>
2004-05-06 10:50:42 +0000

    YACM (yet another cosmetic modification)

--- zipscript/src/datacleaner.c
+++ zipscript/src/datacleaner.c
@@ -67,7 +67,7 @@ void check_dir_loop(char *path) {
 int main (int argc, char **argv) {
 	if (geteuid() != 0) {
 		printf("%s: you can only run this program under effective UID 0.\n", argv[0]);
-		printf("\t(read README.datacleaner for information on how to change effective UID)\n");
+		printf("%s: (read README.datacleaner for information on how to change effective UID)\n", argv[0]);
 		return 1;
 	} else {
 		zd_length = strlen(storage);
