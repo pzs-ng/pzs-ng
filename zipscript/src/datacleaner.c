70261b7f2bf5d22247dbe7d7b9ba37f4f0808993
daxxar <daxxar@pzs-ng.com>
2004-05-06 10:43:13 +0000

    made datacleaner only run as euid 0. also added a README for it.

--- zipscript/src/datacleaner.c
+++ zipscript/src/datacleaner.c
@@ -18,55 +18,61 @@ int		zd_length;
 #endif
 
 void remove_dir_loop(char *path) {
- struct dirent	**list;
- int		n;
- char		target[PATH_MAX];
+	struct dirent	**list;
+	int		n;
+	char		target[PATH_MAX];
 
- chdir(path);
- n = scandir(path, &list, 0, 0);
- while ( n-- ) if ( list[n]->d_name[0] != '.' ) {
-  stat(list[n]->d_name, &entry_stat);
-  if ( S_ISDIR(entry_stat.st_mode) ) {
-   sprintf(target, "%s/%s", path, list[n]->d_name);
-   remove_dir_loop(target);
-   rmdir(target);
-   chdir(path);
-  } else unlink(list[n]->d_name);
-  free(list[n]);
- }
- free(list);
+	chdir(path);
+	n = scandir(path, &list, 0, 0);
+	while ( n-- ) if ( list[n]->d_name[0] != '.' ) {
+		stat(list[n]->d_name, &entry_stat);
+		if ( S_ISDIR(entry_stat.st_mode) ) {
+			sprintf(target, "%s/%s", path, list[n]->d_name);
+			remove_dir_loop(target);
+			rmdir(target);
+			chdir(path);
+		} else unlink(list[n]->d_name);
+		free(list[n]);
+	}
+	free(list);
 }
 
 
 void check_dir_loop(char *path) {
- struct dirent	**list;
- int		n;
- char		target[PATH_MAX];
+	struct dirent	**list;
+	int		n;
+	char		target[PATH_MAX];
  
- chdir(path);
- n = scandir(path, &list, 0, 0);
- while ( n-- ) if ( list[n]->d_name[0] != '.' ) {
-  stat(list[n]->d_name, &entry_stat);
-  if ( S_ISDIR(entry_stat.st_mode) ) {
-   sprintf(target, "%s/%s", path, list[n]->d_name );
-      if (opendir(target + zd_length)) {
-    check_dir_loop(target);
-    chdir(path);
-   } else {
-    remove_dir_loop(target);
-    rmdir(target);
-    chdir(path);
-   }
-  }
-  free(list[n]);
- }
- free(list);
+	chdir(path);
+	n = scandir(path, &list, 0, 0);
+	while ( n-- ) if ( list[n]->d_name[0] != '.' ) {
+		stat(list[n]->d_name, &entry_stat);
+		if ( S_ISDIR(entry_stat.st_mode) ) {
+			sprintf(target, "%s/%s", path, list[n]->d_name );
+			if (opendir(target + zd_length)) {
+				check_dir_loop(target);
+				chdir(path);
+			} else {
+				remove_dir_loop(target);
+				rmdir(target);
+				chdir(path);
+			}
+		}
+		free(list[n]);
+	}
+	free(list);
 }
 
 
-int main () {
- zd_length = strlen(storage);
- chroot(site_root);
- check_dir_loop(storage);
-  return 0;
+int main (int argc, char **argv) {
+	if (geteuid() != 0) {
+		printf("%s: you can only run this program under effective UID 0.\n", argv[0]);
+		printf("\t(read README.datacleaner for information on how to change effective UID)\n");
+		return 1;
+	} else {
+		zd_length = strlen(storage);
+		chroot(site_root);
+		check_dir_loop(storage);
+	}
+	return 0;
 }
