d10ccd2fb830d71578d994103d2324dc1ed9e2da
daxxar <daxxar@pzs-ng.com>
2004-05-10 13:46:27 +0000

    slight cleanup of "hack"

--- sitebot/extra/bnc-check-hacked.pl
+++ sitebot/extra/bnc-check-hacked.pl
@@ -5,105 +5,88 @@
 # Args : <ncftpls location> <user> <pass> <timeout> <bnc1> [bnc2] ... #
 #######################################################################
 
-#use strict;
+use strict;
 
-#use Time::HiRes qw( gettimeofday );
 require 'sys/syscall.ph';
 
 use Net::Ping;
 
-die( "Wrong arguments specified?\n" ) unless ( @ARGV == 5 ); 
+die("Wrong argument-number recieved.\n") unless (@ARGV == 5); 
 
-my( $ncftpls, $username, $password, $timeout, @BNCs, $country, $host, $port, $reply, $tiStart, $tiFinish, $logintime, $i, $error, @pinginfo, $p, @tiStart, @tiFinish );
+my ($ncftpls, $username, $password, $timeout) = @ARGV;
+my @BNCs = split(/ /, $ARGV[4]);
 
-$i = 0;
+my $TIMEVAL_T = "LL";
 
-($ncftpls, $username, $password, $timeout) = @ARGV;
-
-@BNCs = 	split( / /, $ARGV[4] );
-$TIMEVAL_T = "LL";
-
+my $i = 0;
 foreach (@BNCs) {					# for each bouncer
 	$i++;
-	if ( m/[a-zA-Z]+:[^:]+:[^:]+/ ) {	# bouncer format check
-		($country, $host, $port) = split(/:/, $_);
-	} else {
-		print( "$i. Bouncer entry '$_' is not using correct syntax (countrycode:host:port)\n" );
+
+	if (!/[a-zA-Z]+:[^:]+:[^:]+/) {	# bouncer format check
+		print("Bouncer entry #$i ('$_') is not using correct syntax (countrycode:host:port)\n");
 		next;
 	}
 
-	$tiStart = $tiFinish = pack($TIMEVAL_T, ());
+	my ($country, $host, $port) = split(/:/, $_);
+
+	my $tiStart = my $tiFinish = pack($TIMEVAL_T, ());
 	syscall(&SYS_gettimeofday, $tiStart, 0) != -1 or die "gettimeofday: $!";
 
-#	$tiStart = gettimeofday;		# time the ncftpls execution
-	$reply = `$ncftpls -P $port -u $username -p $password -t $timeout -r 0 ftp://$host 2>&1`;
+	my $reply = `$ncftpls -P $port -u $username -p $password -t $timeout -r 0 ftp://$host 2>&1`;
 	
 	syscall(&SYS_gettimeofday, $tiFinish, 0) != -1 or die "gettimeofday: $!";
-	@tiStart = unpack($TIMEVAL_T, $tiStart);
-	@tiFinish  = unpack($TIMEVAL_T, $tiFinish);
+	my @tiStart = unpack($TIMEVAL_T, $tiStart);
+	my @tiFinish = unpack($TIMEVAL_T, $tiFinish);
 
 	# fix microseconds
 	for ($tiFinish[1], $tiStart[1]) { $_ /= 1_000_000 }
-		$logintime = sprintf "%.4f", (($tiFinish[0]  + $tiFinish[1]  ) - ($tiStart[0] + $tiStart[1] )) * 100;
+	my $logintime = sprintf "%.4f", (($tiFinish[0]  + $tiFinish[1]  ) - ($tiStart[0] + $tiStart[1] )) * 100;
 
-#	$tiFinish = gettimeofday;
-#	$logintime = ($tiFinish - $tiStart) * 1000;	# work out how many ms it took to login
-
-	if ($?) {						# returned an error code...so pattern match the
+	if ($?) {					# returned an error code...so pattern match the
 								# STDOUT & STDERR in order to find out what was
 								# wrong, made slightly more complex than needed
 								# as ncftpls' error codes for login issues aren't
 								# that useful.
-		if ( $reply =~ m/username and\/or password was not accepted for login\./ )
-
-		{
+		my $error;
+		$_ = $reply; # heh, nasty but removes $reply =~ on the ifs ;)
+		if (/username and\/or password was not accepted for login\./) {
 			$error = "Couldn't login";
-		}
-		elsif ( $reply =~ m/Connection refused\./ )
-		{
-			$error = "Connection Refused";
-		}
-		elsif ( $reply =~ m/try again later: Connection timed out\./ )
-		{
-			$error = "Connection Timed Out";
-		}
-		elsif ( $reply =~ m/timed out while waiting for server response\./ )
-		{
+		} elsif (/Connection refused\./) {
+			$error = "Connection refused";
+		} elsif (/try again later: Connection timed out\./) {
+			$error = "Connection timed out";
+		} elsif (/timed out while waiting for server response\./) {
 			$error = "No response"
-		}
-		elsif ( $reply =~ m/Remote host has closed the connection\./ )
-		{
-			$error = "Connection Lost";
-		}
-		elsif ( $reply =~ m/unknown host./ )
-		{
-			$error = "Unknown Host?";
-		}
-		else {
-			$error = "Unhandled Error Type?";
+		} elsif (/Remote host has closed the connection\./) {
+			$error = "Connection lost";
+		} elsif (/unknown host\./) {
+			$error = "Unknown host?";
+		} elsif (/Remote host has closed the connection\./) {
+			$error = "Connection lost";
+		} elsif (/unknown host./) {
+			$error = "Unknown host?";
+		} else {
+			$error = "Unhandled error-type?";
 		}
 		print( "$i. .$country - $host:$port - DOWN ($error)\n" );
 
 	} else {						# returned 0, so presuming all was well...
-
-		$p = Net::Ping->new("tcp");	# so let's ping the host to get another
-#		$p->hires();					# bouncer speed indicator
+		my $p = Net::Ping->new("tcp");	# so let's ping the host to get another time ;-)
 
 
 		$tiStart = $tiFinish = pack($TIMEVAL_T, ());
 		syscall(&SYS_gettimeofday, $tiStart, 0) != -1 or die "gettimeofday: $!";
 
-		@pinginfo = $p->ping( $host, 1 );
+		$p->ping($host, 1);
 
 		syscall(&SYS_gettimeofday, $tiFinish, 0) != -1 or die "gettimeofday: $!";
 		@tiStart = unpack($TIMEVAL_T, $tiStart);
-		@tiFinish  = unpack($TIMEVAL_T, $tiFinish);
+		@tiFinish = unpack($TIMEVAL_T, $tiFinish);
 
 		# fix microseconds
 		for ($tiFinish[1], $tiStart[1]) { $_ /= 1_000_000 }
-			$pingtime = sprintf "%.4f", (($tiFinish[0]  + $tiFinish[1]  ) - ($tiStart[0] + $tiStart[1] )) * 100;
+		my $pingtime = sprintf "%.0f", (($tiFinish[0] + $tiFinish[1]) - ($tiStart[0] + $tiStart[1])) * 100;
 
-		printf( "%d. .%s - %s - UP (login: %sms, ping: %sms)\n", $i, $country, "$host:$port", $logintime, $pingtime);
-
+		printf("%d. .%s - %s - UP (login: %sms, ping: %sms)\n", $i, $country, "$host:$port", $logintime, $pingtime);
 	}
 }
