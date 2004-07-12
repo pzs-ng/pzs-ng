04f778ded9960a2b3dd7a7c548d36b2b6287d39b
psxc <psxc@pzs-ng.com>
2004-07-12 23:05:44 +0000

    sitewho should work again... this should be worked on btw - moved to main src dir perhaps...

---
+++ sitewho/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+file="sitewho.c"
+
+echo -n "Compiling $file : "
+gcc -O2 -static -Wall -o ${file%.c} $file 2> /dev/null
+if [ $? -gt 0 ] ; then
+ echo "[FAIL]"
+ exit 2
+fi
+echo "[OK]"
+
+echo ""
+echo "Installing...";
+conf="${file%.c}.conf"
+for v in glrootpath headerfile footerfile; do
+ eval $v=$(grep "^$v" $conf | awk {'print $3'})
+done
+
+cp -f ${headerfile##*/} $glrootpath$headerfile
+cp -f ${footerfile##*/} $glrootpath$footerfile
+for f in ${file%.c} $conf bandwidth.sh; do
+ cp $f $glrootpath/bin/
+done
+
+echo ""
+echo "Check README, if you want to learn how this works!"
+echo ""
--- sitewho/sitewho.c
+++ sitewho/sitewho.c
@@ -9,7 +9,21 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <ctype.h>
-#include "../zipscript/include/objects.h"
+
+struct ONLINE {
+ char                  tagline[64];            /* The users tagline */
+ char                  username[24];           /* The username of the user */
+ char                  status[256];            /* The status of the user, idle, RETR, etc */
+ short int             ssl_flag;               /* 0 = no ssl, 1 = ssl on control, 2 = ssl on control and data */
+ char                  host[256];              /* The host the user is comming from (with ident) */
+ char                  currentdir[256];        /* The users current dir (fullpath) */
+ long                  groupid;                /* The groupid of the users primary group */
+ time_t                login_time;             /* The login time since the epoch (man 2 time) */
+ struct timeval        tstart;                 /* replacement for last_update. */
+ struct timeval        txfer;                  /* The time of the last succesfull transfer. */
+ unsigned long long    bytes_xfer;             /* bytes transferred so far. */
+ pid_t                 procid;                 /* The processor id of the process */
+};
 
 struct GROUP {
 	char		*name;
