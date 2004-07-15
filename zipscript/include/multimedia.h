08a1d333a14b638fe4d838d0156a6ea57d4df6ac
iwdisb <iwdisb@pzs-ng.com>
2004-07-15 22:54:12 +0000

    update headers...forgot them to commit since a few revisions

--- zipscript/include/multimedia.h
+++ zipscript/include/multimedia.h
@@ -4,7 +4,7 @@
 char *trim (char *);
 void mpeg_video(char *, struct video *);
 void avi_video(char *, struct video *);
-//char* get_preset(char [4]); -- this is mostly for internal use, afaict
+/*char* get_preset(char [4]); -- this is mostly for internal use, afaict*/
 void get_mpeg_audio_info(char *, struct audio *);
 
 #endif
--- zipscript/include/stats.h
+++ zipscript/include/stats.h
@@ -1,7 +1,7 @@
 #ifndef _STATS_H_
 #define _STATS_H_
 
-void updatestats(struct VARS *, struct USERINFO **, struct GROUPINFO **, char *, char *, unsigned int, int, int);
+void updatestats(struct VARS *, struct USERINFO **, struct GROUPINFO **, char *, char *, off_t, unsigned int, unsigned int);
 void sortstats(struct VARS *, struct USERINFO **, struct GROUPINFO **);
 void showstats(struct VARS *, struct USERINFO **, struct GROUPINFO **);
 void get_stats(struct VARS *, struct USERINFO **);
