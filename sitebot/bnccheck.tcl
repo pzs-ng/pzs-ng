a585e77069a04d6bb14649f6262e916db13b4ed8
themolest <themolest@pzs-ng.com>
2004-09-04 20:02:17 +0000

    simple fix for bnccheck.tcl ... time to fix ssl

--- sitebot/bnccheck.tcl
+++ sitebot/bnccheck.tcl
@@ -7,9 +7,7 @@ set pass "b0t.be.i"
 proc ftpcheck {host port user pass} {
 	set timeout "false"
 	catch { set sock [socket -async $host $port] }
-	puts "sock=$sock"
 	if { $sock == "sock7" } { return "Error: Connection Refused" }
-#	fconfigure $sock -blocking no
 	set start [clock clicks -milli]
 	while {[flush $sock] == "" && [fblocked $sock]} { 
 		if {[clock clicks -milli] > $start + 2000} { 
@@ -22,13 +20,12 @@ proc ftpcheck {host port user pass} {
 	if { $timeout != "true" } {
 		while {![eof $sock]} {
 			set line [gets $sock]
-			puts "line=$line"
-			set numeric [lindex [split $line] 0]
-			puts "numeric=$numeric"
+			puts "$line"
+			set numeric [string trim [lindex [split $line] 0] "-"]
 			if {[string trim $numeric "0123456789"] != ""} {set numeric "" }
 			switch $numeric {
-				220 { puts "USER $user" ; puts $sock "USER $user" }
-				331 { puts "PASS $pass" ; puts $sock "PASS $pass" }
+				220 { puts "USER $user" ; puts $sock "USER $user" ; flush $sock }
+				331 { puts "PASS $pass" ; puts $sock "PASS $pass" ; flush $sock }
 				230 { close $sock ; return "logged in" }
 				530 { close $sock ; return "failed login -user" }
 				default  { close $sock ; return "failed login" }
