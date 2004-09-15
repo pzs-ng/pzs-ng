7fc285a55abbf67a428301b23399b3c26687002b
psxc <psxc@pzs-ng.com>
2004-09-15 16:14:59 +0000

    added 'sub[s]' and 'subtitles' to the subdir thingy ala cd/dvd/dis[ck]

--- zipscript/src/postdel.c
+++ zipscript/src/postdel.c
@@ -77,8 +77,10 @@ void getrelname(char *directory) {
     if (( ! strncasecmp(path[1], "CD"  , 2) && l[1] <= 4 ) ||
 	    ( ! strncasecmp(path[1], "DISC", 4) && l[1] <= 6 ) ||
 	    ( ! strncasecmp(path[1], "DISK", 4) && l[1] <= 6 ) ||
-	    ( ! strncasecmp(path[1], "DVD" , 3) && l[1] <= 5 )) {  
-	raceI.misc.release_name = malloc( l[0] + 18 );
+	    ( ! strncasecmp(path[1], "DVD" , 3) && l[1] <= 5 ) ||
+            ( ! strncasecmp(path[1], "SUB" , 3) && l[1] <= 4 ) ||
+            ( ! strncasecmp(path[1], "SUBTITLES" , 9) && l[1] <= 9 )) {
+   	raceI.misc.release_name = malloc( l[0] + 18 );
 	sprintf(raceI.misc.release_name, "%s/%s", path[0], path[1]);
 	locations.incomplete = c_incomplete(incomplete_cd_indicator, path);
 	if (k < 2) free(path[1]);
--- zipscript/src/zipscript-c.c
+++ zipscript/src/zipscript-c.c
@@ -92,7 +92,9 @@ void getrelname(char *directory) {
     if (( ! strncasecmp(path[1], "CD"  , 2) && l[1] <= 4 ) ||
 	    ( ! strncasecmp(path[1], "DISC", 4) && l[1] <= 6 ) ||
 	    ( ! strncasecmp(path[1], "DISK", 4) && l[1] <= 6 ) ||
-	    ( ! strncasecmp(path[1], "DVD" , 3) && l[1] <= 5 )) {
+	    ( ! strncasecmp(path[1], "DVD" , 3) && l[1] <= 5 ) ||
+	    ( ! strncasecmp(path[1], "SUB" , 3) && l[1] <= 4 ) ||
+	    ( ! strncasecmp(path[1], "SUBTITLES" , 9) && l[1] <= 9 )) {
 	raceI.misc.release_name = malloc(l[0] + 18);
 	locations.link_source = malloc(n = (locations.length_path - l[1]));
 	sprintf(raceI.misc.release_name, "%s/%s", path[0], path[1]);
