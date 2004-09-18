cc53a044e2334fa9f33a6282e859e47b9d2c4b71
daxxar <daxxar@pzs-ng.com>
2004-09-18 12:25:34 +0000

    removed trunk sitewho, importing from -themeing

--- sitewho/bandwidth.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-glrootpath=/glftpd
-FLAGS="3"
-export FLAGS
-
-$glrootpath/bin/sitewho | grep -e "Up:" | grep -e "Dn:" | grep -e "Total:" | awk '{ printf("%i %0.1f %i %0.1f %i %0.1f\n", $3, $5, $8, $10, $13, $15) }'
