c5cd1006ece4770398d856a5044fc8ec192121de
psxc <psxc@pzs-ng.com>
2004-09-26 20:39:35 +0000

    removed zsconfig.ss5.h renamed zsconfig.alternative1.h to -broken added constants.ss5.h

---
+++ zipscript/include/constants.ss5.h
@@ -0,0 +1,154 @@
+#ifndef __CONSTANTS_H_
+#define __CONSTANTS_H_
+
+#define VERSION		"0.7.3 BETA3 (PRE)"
+
+#define F_IGNORED		254
+#define F_BAD			255
+#define F_NFO			253
+#define F_DELETED		0
+#define F_CHECKED		1
+#define F_NOTCHECKED	2
+
+#define TRUE			1
+#define FALSE			0
+
+#define DISABLED		NULL
+
+#define FILE_MAX		256
+
+#ifndef O_SYNC
+# define O_SYNC O_FSYNC
+#endif
+
+#define video_announce_norace_complete_type	"COMPLETE"
+#define zip_announce_norace_complete_type	"COMPLETE"
+#define rar_announce_norace_complete_type	"COMPLETE"
+#define other_announce_norace_complete_type	"COMPLETE"
+#define audio_announce_norace_complete_type	"COMPLETE"
+#define general_announce_norace_complete_type	"COMPLETE"
+#define video_announce_race_complete_type	"COMPLETE"
+#define zip_announce_race_complete_type		"COMPLETE"
+#define rar_announce_race_complete_type		"COMPLETE"
+#define other_announce_race_complete_type	"COMPLETE"
+#define audio_announce_race_complete_type	"COMPLETE"
+#define general_announce_race_complete_type	"COMPLETE"
+#define audio_announce_vbr_update_type		"UPDATE"
+#define audio_announce_cbr_update_type		"UPDATE"
+#define rar_announce_update_type		"UPDATE"
+#define zip_announce_update_type		"UPDATE"
+#define other_announce_update_type		"UPDATE"
+#define video_announce_update_type		"UPDATE"
+#define general_announce_update_type		"UPDATE"
+#define general_race_type			"RACE"
+#define general_newleader_type			"NEWLEADER"
+#define general_halfway_type			"HALFWAY"
+#define general_doublesfv_type			"DOUBLESFV"
+#define general_sfv_type			"SFV"
+#define general_badgenre_type			"BADGENRE"
+#define general_badyear_type			"BADYEAR"
+#define general_badbitrate_type			"BADBITRATE"
+#define general_incomplete_type			"INCOMPLETE"
+#define stat_users_head_type			"STATS"
+#define stat_users_type				"STATS"
+#define stat_groups_head_type			"STATS"
+#define stat_groups_type			"STATS"
+#define stat_post_type				"STATS"
+
+#define deny_double_msg		"{%U} {%G} {%r} {%n}"
+#define audio_cbr_warn_msg	"{%U} {%G} {%X}"
+#define audio_year_warn_msg	"{%U} {%G} {%Y}"
+#define audio_genre_warn_msg	"{%U} {%G} {%w}"
+
+#define pre_stats		"UserTop: "
+#define between_stats		"GroupTop: "
+#define post_stats		DISABLED
+#define winner			DISABLED
+#define loser			DISABLED
+
+#define user_top		"%n/%u/%g/%.1m/%f/%.1p/%.0s"
+#define user_info		"%u/%g/%.1m/%f/%.1p/%.0s"
+#define group_top		"%n/SS/%g/%.1m/%f/%.1p/%.0s"
+#define group_info		"SS/%g/%.1m/%f/%.1p/%.0s"
+
+/* Special reformat cookies */
+#define fastestfile		"{%u} {%g} {%.3F}" /* Put in %l cookie */
+#define slowestfile		"{%u} {%g} {%.3S}" /* Put in %L cookie */
+#define racersplit		" " /* Put between racers in the %R cookie - !!MUST BE ONLY ONE (1) CHAR!! */
+#define racersmsg		"%u/%g" /* Put in %R cookie */
+#define audio_vbr		"VBR %i %I" /* Put in %j cookie */
+#define audio_cbr		"CBR"    /* Put in %j cookie */
+
+/* Audio files - currently used only for mp3 (mpeg2 layer3), but mpeg1 & mpeg2.5 + layer1 & 2 can be added if needed */
+#define audio_race		"%U %G %.0S {%R}"
+#define audio_sfv		"%f"
+#define audio_update		"{%.1e} {%w} {%Y} {%X} {%j}"
+#define audio_halfway		"{%C0} {%c0}"
+#define audio_newleader		"{%C0}"
+#define audio_complete		"{%U} {%G} {%.0m} {%d} {%.0A}"
+#define audio_norace_halfway	"{%C0} {%c0}"
+#define audio_norace_complete	"{%U} {%G} {%.0m} {%d} {%.0A}"
+
+/* Video files - supports mpeg1/mpeg2 and various avi formats */
+#define video_race		"%U %G %.0S {%R}"
+#define video_sfv		"%f"
+#define video_update		"%.1e %f"
+#define video_halfway		"{%C0} {%c0}"
+#define video_newleader		"{%C0}"
+#define video_complete		"{%U} {%G} {%.0m} {%d} {%.0A}"
+#define video_norace_halfway	"{%C0} {%c0}"
+#define video_norace_complete	"{%U} {%G} {%.0m} {%d} {%.0A}"
+
+/* RAR - allows usage of compression mode cookie (%Z) for rarred releases */
+#define rar_race		"%U %G %.0S {%R}"
+#define rar_sfv			"%f"
+#define rar_update		"%.1e %f"
+#define rar_halfway		"{%C0} {%c0}"
+#define rar_newleader		"{%C0}"
+#define rar_complete		"{%U} {%G} {%.0m} {%d} {%.0A}"
+#define rar_norace_halfway	"{%C0} {%c0}"
+#define rar_norace_complete	"{%U} {%G} {%.0m} {%d} {%.0A}"
+
+
+/* Rest of the stuff inside sfv */
+#define other_race		"%U %G %.0S {%R}"
+#define other_sfv		"%f"
+#define other_update		"%.1e %f"
+#define other_halfway		"{%C0} {%c0}"
+#define other_newleader		"{%C0}"
+#define other_complete		"{%U} {%G} {%.0m} {%d} {%.0A}"
+#define other_norace_halfway	"{%C0} {%c0}"
+#define other_norace_complete	"{%U} {%G} {%.0m} {%d} {%.0A}"
+
+/* Zipped files */
+#define zip_race		"%U %G %.0S {%R}"
+#define zip_update		"%.1e %f"
+#define zip_halfway		"{%C0} {%c0}"
+#define zip_newleader		"{%C0}"
+#define zip_complete		"{%U} {%G} {%.0m} {%d} {%.0A}"
+#define zip_norace_halfway	"{%C0} {%c0}"
+#define zip_norace_complete	"{%U} {%G} {%.0m} {%d} {%.0A}"
+
+/* Get user day/week/month/allup position */
+#define get_user_stats		TRUE
+
+#define show_user_info		TRUE
+#define show_group_info		TRUE
+
+/* Output of racestats binary */
+#define stats_line		"%r/%F/%f%/%u/%g/%C0"
+
+/* Output of postdel binary */
+#define incompletemsg		"%U/%G/%r"
+
+enum ReleaseTypes {
+	RTYPE_NULL = 0,
+	RTYPE_RAR = 1,
+	RTYPE_OTHER = 2,
+	RTYPE_AUDIO = 3,
+	RTYPE_VIDEO = 4,
+	RTYPE_INVALID,
+};
+
+#endif
+
