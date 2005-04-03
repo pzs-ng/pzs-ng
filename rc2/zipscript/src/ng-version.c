15e3c47ad7543d1e5f099030bddcaff392f39cbe
psxc <psxc@pzs-ng.com>
2005-04-03 18:54:55 +0000

    Mark 1602 as RC2

--- rc2/zipscript/src/ng-version.c
+++ rc2/zipscript/src/ng-version.c
@@ -1,4 +1,4 @@
 #include "ng-version.h"
 
-const char* ng_version(void) { const char* NG_Version = "1.0RC2 (r1595)"; return NG_Version; }
+const char* ng_version(void) { const char* NG_Version = "1.0RC2 (r1602)"; return NG_Version; }
 
