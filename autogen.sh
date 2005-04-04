6342ec0ce11fde803184daccda49f634199b6517
js <js@pzs-ng.com>
2005-04-04 15:08:23 +0000

    made autogen.sh more proper

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,46 @@
 #!/bin/sh
 
-# Add checks for different autoconfs here
-valid_autoconf_versions="2.59 2.57 2.5";
+# Add valid autoconf versions here
+autoconf_versions="2.59 2.57 2.5"
 
-autoconf
+for p in `echo $PATH | tr ':' ' '`
+do
+	a=`ls $p/autoconf* 2> /dev/null`
+	autoconf="$autoconf $a"
+done
+
+if [ -n "$autoconf" ]
+then
+	
+	for ac in $autoconf
+	do
+		acv=`$ac --version | awk '/^[Aa]utoconf/{print $NF}'`
+		for ver in $autoconf_versions
+		do
+			if [ "$acv" = "$ver" ]
+			then
+				use_autoconf=$ac
+				break 2
+			fi
+		done
+	done
+
+	# run valid autoconf version
+	if [ -n "$use_autoconf" ]
+	then
+		echo -n "Running $use_autoconf "
+		$use_autoconf
+		
+		if [ $? -eq 0 ]; then
+			echo "[OK]"
+		else
+			echo "[FAIL]"
+		fi
+
+		exit 0
+	fi
+	
+fi
+
+echo "No usable autoconf version found"
+
