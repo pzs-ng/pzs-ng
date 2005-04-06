0e7204046193833751c25695893ee9c58efdade7
psxc <psxc@pzs-ng.com>
2005-04-06 10:45:45 +0000

    remove >/dev/null from unzip (temp fix(?))

--- zipscript/src/rescan.c
+++ zipscript/src/rescan.c
@@ -366,7 +366,7 @@ main(void)
 				g.v.total.start_time = 0;
 
 				if (!fileexists("file_id.diz")) {
-					sprintf(exec, "%s -qqjnCLL \"%s\" file_id.diz 2>/dev/null", unzip_bin, g.v.file.name);
+					sprintf(exec, "%s -qqjnCLL \"%s\" file_id.diz 2>.delme", unzip_bin, g.v.file.name);
 					if (execute(exec) != 0) {
 						d_log("rescan: No file_id.diz found (#%d): %s\n", errno, strerror(errno));
 					} else {
@@ -378,7 +378,7 @@ main(void)
 						chmod("file_id.diz", 0666);
 					}
 				}
-				sprintf(exec, "%s -qqt \"%s\" &> /dev/null", unzip_bin, g.v.file.name);
+				sprintf(exec, "%s -qqt \"%s\" >.delme", unzip_bin, g.v.file.name);
 				if (system(exec) == 0) {
 					writerace(g.l.race, &g.v, crc, F_CHECKED);
 				} else {
@@ -452,6 +452,9 @@ main(void)
 	free(g.l.race);
 	free(g.l.sfv);
 	free(g.l.leader);
+
+	if (fileexists(".delme"))
+		unlink(".delme");
 
 	remove_lock(&g.v);
 
