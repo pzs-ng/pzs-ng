7a7a66b1018bbd520c1cede1549211139cdf80fe
psxc <psxc@pzs-ng.com>
2005-04-26 21:11:01 +0000

    buggilbug fix

--- lib/libglufile/gluserfile.c
+++ lib/libglufile/gluserfile.c
@@ -501,7 +501,7 @@ AINT *strtointarray(AINT *target, char *s)
 	}
 	
 	/* contract */
-	target->n = i+1;
+	target->n = i+3;
 	target->val = realloc(target->val, sizeof(int)*(target->n));
 	
 	return target;
