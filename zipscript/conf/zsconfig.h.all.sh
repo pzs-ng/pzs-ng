b93f412c6f5bcd032ec9212092d2cbaa2d2af4d6
psxc <psxc@pzs-ng.com>
2005-05-05 10:43:10 +0000

    created a small script that extract the stuff from zsconfig.defaults.h to the README.ZSCONFIG.

--- zipscript/conf/zsconfig.h.all.sh
+++ zipscript/conf/zsconfig.h.all.sh
@@ -3,13 +3,29 @@
 # small script to create a "full" zsconfig.
 
 if [ -e zipscript/include/zsconfig.defaults.h ]; then
-  cat zipscript/include/zsconfig.defaults.h | grep -e "^#define" -a -e "^ " -a -e "^/" -a -e "^	" >zipscript/conf/zsconfig.h.all
-  echo "zipscript/conf/zsconfig.h.all created."
-elif [ -e ../include/zsconfig.defaults.h ]; then
-  cat ../include/zsconfig.defaults.h | grep -e "^#define" -a -e "^ " -a -e "^/" -a -e "^ " >zsconfig.h.all
-  echo "zsconfig.h.all created."
+  conf=zipscript/include/zsconfig.defaults.h
+  dest=zipscript/conf/zsconfig.h.all
+elif  [ -e ../include/zsconfig.defaults.h ]; then
+  conf=../include/zsconfig.defaults.h
+  dest=zsconfig.h.all
 else
   echo "FAIL: could not find zsconfig.defaults.h."
+  exit 1
 fi
+echo "Creating config file ... this may take a few seconds.. bash is not lighning fast ;P"
+IFS_ORG="$IFS"
+IFS="
+"
+start=0;
+echo -n "" >$dest
+for line in `cat $conf | grep -v "^#ifndef" | grep -v "^#endif" | tr '/' '\^'`; do
+  if [[ $start -eq 0 ]]; then
+    [[ `echo "$line" | grep -e ":START:"` ]] && start=1 && continue;
+  else
+    [[ `echo "$line" | grep -e ":END:"` ]] && start=0 && continue;
+  fi
+  [[ $start -eq 1 ]] && echo $line | tr '\^' '/' >>$dest
+done
+echo "$dest created."
 exit 0
 
