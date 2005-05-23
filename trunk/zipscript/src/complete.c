7427563c33490d699e7c74dc817c24ded756d246
psxc <psxc@pzs-ng.com>
2005-05-23 12:09:17 +0000

    true fix for the ss5 thingy

--- trunk/zipscript/src/complete.c
+++ trunk/zipscript/src/complete.c
@@ -140,8 +140,8 @@ writetop(GLOBAL *g, int completetype)
 			pbuf = buffer = ng_realloc(buffer, FILE_MAX, 1, 1, &g->v, 1);
 			for (cnt = 0; cnt < max_users_in_top && cnt < g->v.total.users; cnt++) {
 				snprintf(templine, FILE_MAX, "%s ", convert2(&g->v, g->ui[g->ui[cnt]->pos], g->gi, user_top, cnt));
-				mlen += strlen(templine);
-				if (mlen >= FILE_MAX * mset) {
+				mlen = strlen(templine);
+				if ((int)strlen(buffer) + mlen >= FILE_MAX * mset) {
 					mset += 1;
 					buffer = ng_realloc(buffer, FILE_MAX * mset, 1, 1, &g->v, 0);
 				}
@@ -158,8 +158,8 @@ writetop(GLOBAL *g, int completetype)
 			pbuf = buffer = ng_realloc(buffer, FILE_MAX, 1, 1, &g->v, 1);
 			for (cnt = 0; cnt < max_groups_in_top && cnt < g->v.total.groups; cnt++) {
 				snprintf(templine, FILE_MAX, "%s ", convert3(&g->v, g->gi[g->gi[cnt]->pos], group_top, cnt));
-				mlen += strlen(templine);
-				if (mlen >= FILE_MAX * mset) {
+				mlen = strlen(templine);
+				if ((int)strlen(buffer) + mlen >= FILE_MAX * mset) {
 					mset += 1;
 					buffer = ng_realloc(buffer, FILE_MAX * mset, 1, 1, &g->v, 0);
 				}
