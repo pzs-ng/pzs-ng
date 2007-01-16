255b47abc8dcf49498360241b6bc401d5322610b
psxc <psxc@pzs-ng.com>
2007-01-16 20:18:07 +0000

    added audio_artist_nosub - instf creating a one-letter subdir this option will create a dir with the complete artist name. Still need a little work (like changing spaces to dots in the artistname etc).

--- trunk/zipscript/src/audiosort.c
+++ trunk/zipscript/src/audiosort.c
@@ -91,9 +91,16 @@ if (subcomp(link_target, NULL)) {
 		if (!strncasecmp(link_target, "VA", 2) && (link_target[2] == '-' || link_target[2] == '_'))
 			memcpy(info->id3_artist, "VA", 3);
 		if (memcmp(info->id3_artist, "VA", 3)) {
+#if ( audio_artist_nosub == FALSE )
 			temp_p = ng_realloc(temp_p, 2, 1, 0, NULL, 1);
 			snprintf(temp_p, 2, "%c", toupper(*info->id3_artist));
 			createlink(audio_artist_path, temp_p, link_source, link_target);
+#else
+			temp_p = ng_realloc(temp_p, strlen(info->id3_artist)+1, 1, 0, NULL, 1);
+			snprintf(temp_p, strlen(info->id3_artist)+1, "%s", info->id3_artist);
+			temp_p = check_nocase_linkname(audio_artist_path, temp_p);
+			createlink(audio_artist_path, temp_p, link_source, link_target);
+#endif
 			ng_free(temp_p);
 		} else
 			createlink(audio_artist_path, "VA", link_source, link_target);
