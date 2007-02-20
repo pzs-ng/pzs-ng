91e012c451aa498f78c8ed261d94daf676ba69c2
comp <comp@pzs-ng.com>
2007-02-20 02:00:35 +0000

    fix fuckup

--- trunk/sitebot/extra/ng-topic.sh
+++ trunk/sitebot/extra/ng-topic.sh
@@ -3,7 +3,7 @@
 gllog="/ftp-data/logs/glftpd.log"
 channel=$1
 topic=`echo $@ | sed "s/$1 //g"`
-if [ "$1" == "#penis-beta" ] || [ "$1" == "#penis-staff" ] || [ "$1" == "#penis-sups" ]; then
+if [ "$channel" == "#chan" ]; then
     echo `date "+%a %b %e %T %Y"` SETTOPIC: \"$channel\" \"$topic\" >> $gllog
     echo "Sent topic ($topic) to $channel"
 else
