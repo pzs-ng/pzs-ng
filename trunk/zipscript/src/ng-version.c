28a417bd07b03dd88b7ad646c0cf67d43f241d78
psxc <psxc@pzs-ng.com>
2007-04-09 12:47:41 +0000

    marked as v1.0.9

--- trunk/zipscript/src/ng-version.c
+++ trunk/zipscript/src/ng-version.c
@@ -1,3 +1,3 @@
 #include "ng-version.h"
 
-const char* ng_version(void) { const char* NG_Version = "1.0.X.r2222"; return NG_Version; }
+const char* ng_version(void) { const char* NG_Version = "1.0.X.r2291"; return NG_Version; }
