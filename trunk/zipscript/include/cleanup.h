c72cd409682bc2fe3279fa959ee229f3c5bd9125
daxxar <daxxar@pzs-ng.com>
2007-04-24 12:54:04 +0000

    possibly better errorhandling in cleanup, for regcomp

--- trunk/zipscript/include/cleanup.h
+++ trunk/zipscript/include/cleanup.h
@@ -6,6 +6,7 @@ char *replace_cookies(char *);
 void incomplete_cleanup(char *, int, char *);
 void cleanup(char *, char *, int, char *);
 int checklink(char *, char *, int);
+int regcomp_error(int, regex_t *, char *);
 
 #endif
 
