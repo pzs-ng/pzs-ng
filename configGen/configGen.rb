843af9a2bb67b4dfc54aa6db70babd6141e3bcb6
daxxar <daxxar@pzs-ng.com>
2007-06-23 14:32:22 +0000

    Added new system for us developers to manage README.ZSCONFIG, zsconfig.h.dist, errors.h and zsconfig.defaults.h. It's called "configGen", and it reads from config.yaml - the file is mostly self-explanatory. Updated README.ZSCONFIG, zsconfig.defaults.h, errors.h and zsconfig.h.dist from new config.yaml. Added new parameter to zipscript-c: config. This will just print the defines used when compiling this. (Perhaps add some kinda feature to see if the option is default value, and only print non-defaults?)

---
+++ configGen/configGen.rb
@@ -0,0 +1,98 @@
+#!/usr/bin/ruby
+require 'yaml'
+
+Dir.chdir(File.dirname(__FILE__))
+
+def get_default(option)
+    if option['type'] == 'boolean'
+        option['default'].to_s.upcase
+    else
+        option['default']
+    end
+end
+def get_readme_typestring(option)
+    type = case option['type']
+    when 'integer'
+        if option.has_key? 'valid_values' and option['valid_values'].is_a? Array
+            # We should eventually support e.g. ranges and such.
+            option['valid_values'].join '|'
+        else
+            'NUMBER'
+        end
+    when 'boolean'
+        'TRUE|FALSE'
+    when 'character'
+        'CHAR'
+    else
+        option['type'].upcase
+    end
+    if option.has_key? 'can_disable' and option['can_disable']
+        type += '|DISABLED'
+    end
+    
+    type
+end
+
+config = YAML::load_file 'config.yaml'
+
+max_length = config['options'].keys.max {|a,b| a.length <=> b.length }.length
+max_length_dist = config['dist_options'].max {|a,b| a.to_s.length <=> b.to_s.length }.length
+max_length_err = config['err_options'].max {|a,b| a.to_s.length <=> b.to_s.length }.length
+
+File.open('../README.ZSCONFIG', 'w') do |file|
+    file.puts config['readme_header']
+    config['options'].sort.each do |name, info|
+        file.puts "#{name} <#{get_readme_typestring info}>", info['comment'].gsub(/^/, "\t")
+        file.puts "\tDefault: #{get_default info}", ''
+    end
+end
+File.open('../zipscript/include/zsconfig.defaults.h', 'w') do |file|
+    file.puts config['defaults_header']
+    config['options'].sort.each do |name, info|
+        next if config['err_options'].include? name
+        file.puts "#ifndef #{name}", "#define #{sprintf "% -#{max_length}s", name} #{get_default info}", "#endif", ''
+    end
+end
+File.open('../zipscript/include/errors.h', 'w') do |file|
+    config['err_options'].each do |name|
+        info = config['options'][name]
+        file.puts "#ifndef #{name}", "#define #{sprintf "% -#{max_length_err}s", name} #{get_default info}", "#endif", ''
+    end
+end
+File.open('../zipscript/conf/zsconfig.h.dist', 'w') do |file|
+    file.puts config['dist_header']
+    config['dist_options'].each do |name|
+        if name.nil?
+            file.puts ''
+        else
+            info = config['options'][name]
+            file.puts "#define #{sprintf "% -#{max_length_dist}s", name} #{get_default info}"
+        end
+    end
+end
+
+File.open('../zipscript/src/print_config.c', 'w') do |file|
+    file.puts '#include <stdio.h>', ''
+    file.puts '#include "constants.h"'
+    file.puts '#include "print_config.h"', ''
+    file.puts '#include "../conf/zsconfig.h"'
+    file.puts '#include "zsconfig.defaults.h"', ''
+    file.puts '#include "errors.h"'
+
+    file.puts '/* Hardcore preprocessor hackery, thanks to runehol/#scene.no. */'
+    file.puts '#define expand(x) x'
+    file.puts '#define stringize(x) expand(#x)'
+    file.puts '#define stringify(x) stringize(x)'
+
+    file.puts 'void print_config(void)', '{'
+    config['options'].sort.each do |name, info|
+        file.print "    printf(\"#define #{sprintf "% -#{max_length}s", name} %s\\n\", "
+        if info['type'] == 'boolean'
+            file.print "(#{name} == FALSE ? \"FALSE\" : \"TRUE\")"
+        else
+            file.print "stringify(#{name})"
+        end
+        file.puts ");"
+    end
+    file.puts '}'
+end
--- zipscript/include/errors.h
+++ zipscript/include/errors.h
@@ -1,51 +1,60 @@
-#include "../conf/zsconfig.h"
+#ifndef BAD_CRC
+#define BAD_CRC        "CRC-Check: BAD!"
+#endif
+
+#ifndef BAD_ZIP
+#define BAD_ZIP        "ZiP-Integrity: BAD!"
+#endif
+
+#ifndef BANNED_BITRATE
+#define BANNED_BITRATE "%s kbit codec is not allowed here!"
+#endif
+
+#ifndef BANNED_FILE
+#define BANNED_FILE    "Banned file: Not allowed!"
+#endif
+
+#ifndef BANNED_GENRE
+#define BANNED_GENRE   "%s is banned here!"
+#endif
+
+#ifndef BANNED_YEAR
+#define BANNED_YEAR    "Releases from %s are not allowed here!"
+#endif
+
+#ifndef DOUBLE_SFV
+#define DOUBLE_SFV     "SFV-file: DUPE!"
+#endif
+
+#ifndef DUPE_NFO
+#define DUPE_NFO       "NFO-File: DUPE!"
+#endif
+
+#ifndef EMPTY_FILE
+#define EMPTY_FILE     "0byte-file: Not allowed!"
+#endif
+
+#ifndef EMPTY_SFV
+#define EMPTY_SFV      "SFV-file: BAD!"
+#endif
+
+#ifndef NOT_IN_SFV
+#define NOT_IN_SFV     "CRC-Check: Not in sfv!"
+#endif
 
 #ifndef SFV_FIRST
-#define SFV_FIRST	"CRC-Check: SFV first!"
+#define SFV_FIRST      "CRC-Check: SFV first!"
 #endif
-#ifndef ZERO_CRC
-#define ZERO_CRC	"CRC-Check: ERROR!"
-#endif
-#ifndef BAD_CRC
-#define BAD_CRC		"CRC-Check: BAD!"
-#endif
-#ifndef NOT_IN_SFV
-#define NOT_IN_SFV	"CRC-Check: Not in sfv!"
-#endif
-#ifndef EMPTY_SFV
-#define EMPTY_SFV	"SFV-file: BAD!"
-#endif
-#ifndef DOUBLE_SFV
-#define DOUBLE_SFV	"SFV-file: DUPE!"
-#endif
-#ifndef BAD_ZIP
-#define BAD_ZIP		"ZiP-Integrity: BAD!"
-#endif
-#ifndef EMPTY_FILE
-#define EMPTY_FILE	"0byte-file: Not allowed!"
-#endif
-#ifndef BANNED_FILE
-#define BANNED_FILE	"Banned file: Not allowed!"
-#endif
-#ifndef UNKNOWN_FILE
-#define UNKNOWN_FILE	"%s-file: Not allowed!"				/* %s = file extension	 */
-#endif
-#ifndef BANNED_GENRE
-#define BANNED_GENRE	"%s is banned here!"				/* %s = genre		 */
-#endif
-#ifndef BANNED_YEAR
-#define BANNED_YEAR	"Releases from %s are not allowed here!"	/* %s = year		 */
-#endif
-#ifndef BANNED_BITRATE
-#define BANNED_BITRATE	"%s kbit codec is not allowed here!"		/* %s = bitrate		 */
-#endif
-#ifndef DUPE_NFO
-#define DUPE_NFO	"NFO-File: DUPE!"
-#endif
-#ifndef ZIP_NFO
-#define ZIP_NFO		"NFO-File: Not allowed here!"
-#endif
+
 #ifndef SPEEDTEST
-#define SPEEDTEST	"BW: %.1fMiB (%.1fMB) @ %.2fMbps (%.2fMB/s)."
-#endif 
+#define SPEEDTEST      "BW: %.1fMiB (%.1fMB) @ %.2fMbps (%.2fMB/s)."
+#endif
+
+#ifndef UNKNOWN_FILE
+#define UNKNOWN_FILE   "%s-file: Not allowed!"
+#endif
+
+#ifndef ZIP_NFO
+#define ZIP_NFO        "NFO-File: Not allowed here!"
+#endif
 
