03b0b693c7469d64d09812ab5395f1dfb2009497
psxc <psxc@pzs-ng.com>
2009-01-25 11:56:38 +0000

    added windows slash to check for invalid chars in sfv (allow_slash_in_sfv)

--- zipscript/src/race-file.c
+++ zipscript/src/race-file.c
@@ -504,12 +504,16 @@ copysfv(const char *source, const char *target, struct VARS *raceI)
 			ptr = find_last_of(ptr, "/") + 1;
 			d_log("copysfv: found '/' in filename - adjusting.\n");
 		}
+		if (ptr != find_last_of(ptr, "\\")) {
+			ptr = find_last_of(ptr, "\\") + 1;
+			d_log("copysfv: found '\\' in filename - adjusting.\n");
+		}
 #endif
 		if (strlen(ptr) > 0 && strlen(ptr) < NAME_MAX-9 ) {
 			strlcpy(sd.fname, ptr, NAME_MAX-9);
 
-			if (sd.fname != find_last_of(sd.fname, "\t/") || *sd.fname == '/') {
-				d_log("copysfv: found '/' or <TAB> as part of filename in sfv - logging file as bad.\n");
+			if (sd.fname != find_last_of(sd.fname, "\t") || sd.fname != find_last_of(sd.fname, "\\") || sd.fname != find_last_of(sd.fname, "/")) {
+				d_log("copysfv: found '/', '\\' or <TAB> as part of filename in sfv - logging file as bad.\n");
 				retval = 1;
 				break;
 			}
