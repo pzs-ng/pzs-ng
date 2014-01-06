af546a0c8cd98c279b708c4bb5686485ce723faa
Sked <Sk@dd.ie>
2014-01-06 01:51:17 +0100

    Fix configure for glftpd 2.03. Add .log to default allowed_types.

--- zipscript/include/zsconfig.defaults.h
+++ zipscript/include/zsconfig.defaults.h
@@ -87,7 +87,7 @@
 
 #ifndef allowed_types
 #define allowed_types_is_defaulted
-#define allowed_types                             "jpg,cue,m3u,txt,vob,png,m2ts,ts"
+#define allowed_types                             "jpg,cue,m3u,txt,vob,png,m2ts,ts,log"
 #endif
 
 #ifndef allowed_types_exemption_dirs
@@ -97,7 +97,7 @@
 
 #ifndef allowed_years
 #define allowed_years_is_defaulted
-#define allowed_years                             "2007,2008,2009,2010,2011,2012"
+#define allowed_years                             "2009,2010,2011,2012,2013,2014"
 #endif
 
 #ifndef always_scan_audio_syms
