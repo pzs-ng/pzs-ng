26ee62cc7bed77b3cb817126242bf9ef21a86198
Sked <Sk@dd.ie>
2018-12-24 01:27:55 +0100

    Fix cleanup and incomplete-list.sh

--- sitebot/extra/incomplete-list.sh
+++ sitebot/extra/incomplete-list.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 ##############################
 # CONFIG                     #
 # ^^^^^^                     #
@@ -13,8 +12,8 @@ glroot=/glftpd
 cleanup=$glroot/bin/cleanup
 
 # enter sections in the following format:
-# <announce name of section>:<path to section, including a terminating slash ``/''
-# spaces and newline separates.
+# <announce name of section>:<chrooted path to section, space separates paths>
+# newline separates sections.
 sections="
 0DAY:/site/incoming/0day/
 GAMES:/site/incoming/games/
@@ -22,20 +21,18 @@ APPS:/site/incoming/apps/
 MV:/site/incoming/musicvideos/
 "
 
-# alternative, set the following variable to point to your dZSbot.conf and
-# uncomment the ''botconf='' directive below.
-#botconf=/path/to/sitebot/pzs-ng/dZSbot.conf
-
-# Set this to your complete line (non-dynamic part)
-releaseComplete="Complete -"
+# alternative, set the following variable to point to your ngBot.conf or
+# other file where you have your section paths defined and uncomment the
+# "botconf=" directive below.
+#botconf=/path/to/sitebot/pzs-ng/ngBot.conf
 
 # set this to 1 if you wish to announce sections where no incompletes are found.
 verbose=0
 
 #bold char
-bold=""
+bold=""
 
-# set this to one if you have sections in subdirs of one another - ie,
+# set this to zero if you have sections in subdirs of one another - ie,
 # if you have defined in $sections "A:/site/DIR" and "B:/site/DIR/SUBDIR"
 no_strict=0
 
@@ -52,29 +49,48 @@ fi;
 IFSORIG="$IFS"
 IFS="
 "
+cleanupresults="`$cleanup $glroot 2>/dev/null | grep -e "^Incomplete" | tr '\"' '\n' | tr -s '/'`"
 for section in $sections; do
   secname="`echo "$section" | cut -d ':' -f 1`"
   secpaths="`echo "$section" | cut -d ':' -f 2- | tr ' ' '\n'`"
 
   for secpath in $secpaths; do
-    results="`$cleanup $glroot 2>/dev/null | grep -e "^Incomplete" | tr '\"' '\n' | grep -e "$secpath" | tr -s '/'`"
+    results="`echo "$cleanupresults" | grep -e "$glroot$secpath"`"
     if [ -z "$results" ]; then
       if [ $verbose -eq 1 ]; then
         echo "$secname: No incomplete releases found."
       fi
     else
       for result in $results; do
-        secrel="`echo $result/ | sed "s|$glroot$path||" | tr -s '/'`"
-        comp="`ls -1 $result/ | grep "$releaseComplete"`"
-        percent="`echo $comp | cut -d ' ' -f 4` complete"
+        percent="`ls -1 "$result/" | sed -n 's/[^0-9]*\([0-9][0-9]*%\).*/\1/p'` complete"
 
         if [ $percent == " complete" ]; then
            percent="incomplete"
         fi
 
-	if [ $no_strict ] || [ "`dirname $secrel`/" = "`echo $secpath/ | tr -s '/'`" ]; then 
+        # check for overlapping section paths
+        # so we don't announce twice
+        pass=1
+        if [ $no_strict -eq 0 ]; then
+          for sctn in $sections; do
+            scpths="`echo "$sctn" | cut -d ':' -f 2- | tr ' ' '\n'`"
+            for scpth in $scpths; do
+              if [ "$secpath" != "$scpth" ] && [ "`echo "$scpth" | grep "^$secpath/"`" ]; then
+                pass=0
+                break
+              fi
+            done
+            if [ $pass -eq 0 ]; then
+              break
+            fi
+          done
+        fi
+
+        if [ $no_strict -eq 1 ] || [ $pass -eq 1 ]; then
+          sedpath=`echo "$glroot$secpath/" | tr -s '/'`
+          secrel="`echo $result | sed "s|$sedpath||" | tr -s '/'`"
           echo "$secname: ${bold}${secrel}${bold} is $percent."
-	fi
+        fi
       done
     fi
  done
--- zipscript/include/cleanup.h
+++ zipscript/include/cleanup.h
@@ -3,9 +3,9 @@
 
 void scandirectory(char *, int);
 char *replace_cookies(char *);
-void incomplete_cleanup(char *, int);
+void incomplete_cleanup(char *, char *, int);
 void cleanup(char *, char *, int, char *);
-int checklink(char *, int);
+int checklink(char *, char *, int);
 int regcomp_error(int, regex_t *, char *);
 short matchpath(char *, char *);
 
